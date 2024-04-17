80ff155d87cde79493f3b77585e3b97f308cd0ae
asianParadissseee <asian.paradissseee@gmail.com>
2024-04-17 20:20:09 +0600

    add tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from "@/app/app"
-
+import "@/app/styles/base.css"
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
     <App/>
