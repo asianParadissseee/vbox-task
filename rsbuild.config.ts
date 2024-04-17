90b682e212493f3c09176b960fa4f79233d3e190
asianParadissseee <asian.paradissseee@gmail.com>
2024-04-17 20:14:52 +0600

    add alias

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,16 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  source: {
+    alias: () => {
+      return {
+        '@/shared': './src/shared',
+        '@/entities': './src/entities',
+        '@/features': './src/features',
+        '@/widgets': './src/widgets',
+        '@/pages': './src/pages',
+        '@app': './src/app',
+      }
+    }
+  }
 });
